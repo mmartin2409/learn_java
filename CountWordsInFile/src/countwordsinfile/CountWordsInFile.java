f14f8e8659e890e96a801e981c1ca99f58754913
Matthew Martin <mmartin2409@gmail.com>
2018-07-02 00:38:13 +1000

    added some missing stuff

---
+++ CountWordsInFile/src/countwordsinfile/CountWordsInFile.java
@@ -0,0 +1,17 @@
+package countwordsinfile;
+
+/**
+ * the following will read a string from a given .txt file and return a word
+ * count. 
+ * @author matthew
+ */
+public class CountWordsInFile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
